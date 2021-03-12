fc716caf5e2b16e1098cf02744f636b90940c53b
George Liontos <sdi1600094@di.uoa.gr>
2021-03-13 00:45:20 +0200

    Added SRS file as well as a helper script to generate table of contents

---
+++ internal/scripts/generate_table_of_contents.py
@@ -0,0 +1,102 @@
+#! /usr/bin/python3
+import argparse
+import sys
+
+
+class Header:
+    def __init__(self, header_level, text):
+        self.header_level = header_level
+        self.text = text
+
+    def to_table_of_contents_intra_link(self):
+        header_number = '#' * self.header_level
+        link = self.text.lower().replace(' ', '-')
+
+        intra_link = f"[{self.text}]({header_number}{link})"
+
+        if self.header_level <= 2:
+            intra_link = f"**{intra_link}**"
+
+        return intra_link
+
+
+def get_header_number(s):
+    i = 0
+    while s[i] == '#':
+        i += 1
+    return i
+
+
+def parse_file_contents(file_contents):
+    table_of_contents = []
+    for line in file_contents.splitlines():
+        if line.startswith('#'):
+            header_number = get_header_number(line)
+            if header_number >= 2:
+                text = str(line[header_number:]).lstrip()
+                table_of_contents.append(Header(header_number, text))
+
+    return table_of_contents
+
+
+def format_table_of_contents(toc):
+    rows = []
+    rows.append("# Table of Contents")
+    for (i, row) in enumerate(toc):
+        intra_link = row.to_table_of_contents_intra_link()
+        if row.header_level == 2:
+            # Hacker :^)
+            if i != 0:
+                rows[i] = rows[i][:len(rows[i]) - 1]
+            intra_link = f"\n{intra_link}"
+        rows.append(f"{intra_link}\\")
+
+    # Hacker :^)
+    rows[i + 1] = rows[i + 1][:len(rows[i + 1]) - 1]
+    rows.append('\n')
+    return '\n'.join(rows)
+
+
+def process_files(cli):
+    for f in cli.file:
+        file_contents = f.read()
+        table_of_contents = parse_file_contents(file_contents)
+        if table_of_contents:
+            table_of_contents = format_table_of_contents(table_of_contents)
+            if cli.update_in_place:
+                f.close()
+                with open(f.name, "w") as f:
+                    f.write(table_of_contents)
+                    f.write(file_contents)
+            else:
+                print(f"----- {f.name} -----")
+                print(table_of_contents)
+
+
+def parse_arguments():
+    parser_arguments = {
+        ("--update-in-place",): {
+            "action": "store_true",
+            "default": False,
+            "help": "Update the files in place with the table of contents",
+        },
+        ("file",): {
+            "type": argparse.FileType('r'),
+            "nargs": "+",
+        }
+    }
+
+    parser = argparse.ArgumentParser()
+    for (arg, arg_info) in parser_arguments.items():
+        parser.add_argument(*arg, **arg_info)
+
+    return parser.parse_args()
+
+
+def main():
+    cli = parse_arguments()
+    process_files(cli)
+
+
+if __name__ == "__main__":
+    main()
---
+++ srs.md
@@ -0,0 +1,81 @@
+# Table of Contents
+
+**[1. Introduction](##1.-introduction)**\
+[1.1 Purpose](###1.1-purpose)\
+[1.2 Document Conventions](###1.2-document-conventions)\
+[1.3 Intented Audience and Reading Suggestions](###1.3-intented-audience-and-reading-suggestions)\
+[1.4 Project Scope](###1.4-project-scope)\
+[1.5 References](###1.5-references)
+
+**[2. Overall Description](##2.-overall-description)**\
+[2.1 Product Perspective](###2.1-product-perspective)\
+[2.2 Product Features](###2.2-product-features)\
+[2.3 User Classes and Characteristics](###2.3-user-classes-and-characteristics)\
+[2.4 Operating Environment](###2.4-operating-environment)\
+[2.5 Design and Implementation Contstraints](###2.5-design-and-implementation-contstraints)\
+[2.6 Assumptions and Dependencies](###2.6-assumptions-and-dependencies)
+
+**[3. System Features](##3.-system-features)**\
+[3.1 Functional Requiremenets](###3.1-functional-requiremenets)
+
+**[4. External Interface Requirements](##4.-external-interface-requirements)**\
+[4.1 User Interfaces](###4.1-user-interfaces)\
+[4.2 Hardware Interfaces](###4.2-hardware-interfaces)\
+[4.3 Software Interfaces](###4.3-software-interfaces)\
+[4.4 Communication Interfaces](###4.4-communication-interfaces)
+
+**[5. Nonfunctional Requirements](##5.-nonfunctional-requirements)**\
+[5.1 Performance Requirements](###5.1-performance-requirements)\
+[5.2 Safety Requirements](###5.2-safety-requirements)\
+[5.3 Security Requirements](###5.3-security-requirements)\
+[5.4 Software Quality Requirements](###5.4-software-quality-requirements)
+
+## 1. Introduction
+
+### 1.1 Purpose
+
+### 1.2 Document Conventions
+
+### 1.3 Intented Audience and Reading Suggestions
+
+### 1.4 Project Scope
+
+### 1.5 References
+
+## 2. Overall Description
+
+### 2.1 Product Perspective
+
+### 2.2 Product Features
+
+### 2.3 User Classes and Characteristics
+
+### 2.4 Operating Environment
+
+### 2.5 Design and Implementation Contstraints
+
+### 2.6 Assumptions and Dependencies
+
+## 3. System Features
+
+### 3.1 Functional Requiremenets
+
+## 4. External Interface Requirements
+
+### 4.1 User Interfaces
+
+### 4.2 Hardware Interfaces
+
+### 4.3 Software Interfaces
+
+### 4.4 Communication Interfaces
+
+## 5. Nonfunctional Requirements
+
+### 5.1 Performance Requirements
+
+### 5.2 Safety Requirements
+
+### 5.3 Security Requirements
+
+### 5.4 Software Quality Requirements
