c7ad11c7a2cc187521ea348d8c91cb784854959f
themisvr <varveristhemis@gmail.com>
2021-04-18 23:05:09 +0300

    fixed deprecated WebMvcConfigurerAdapter

--- back-end/dira/src/main/java/di/uoa/gr/dira/WebMvcConfig.java
+++ back-end/dira/src/main/java/di/uoa/gr/dira/WebMvcConfig.java
@@ -7,14 +7,14 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.persistence.EntityManager;
 import java.util.List;
 
 
 @Configuration
-public class WebMvcConfig extends WebMvcConfigurerAdapter {
+public class WebMvcConfig implements WebMvcConfigurer {
     private final ApplicationContext applicationContext;
     private final EntityManager entityManager;
 
@@ -26,7 +26,6 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
-        super.addArgumentResolvers(argumentResolvers);
         ObjectMapper objectMapper = Jackson2ObjectMapperBuilder.json().applicationContext(this.applicationContext).build();
         argumentResolvers.add(new EntityModelMapper(objectMapper, entityManager));
     }
