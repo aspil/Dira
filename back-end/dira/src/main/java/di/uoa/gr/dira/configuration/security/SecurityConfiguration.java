1c3452566f5b44a9ed11bb97370e1b882392624e
aspil <sdi1700147@di.uoa.gr>
2021-05-11 23:05:33 +0300

    Updated SpringSecurity configuration

--- back-end/dira/src/main/java/di/uoa/gr/dira/configuration/security/SecurityConfiguration.java
+++ back-end/dira/src/main/java/di/uoa/gr/dira/configuration/security/SecurityConfiguration.java
@@ -1,14 +1,32 @@
 package di.uoa.gr.dira.configuration.security;
 
+import di.uoa.gr.dira.security.JwtAuthenticationFilter;
+import di.uoa.gr.dira.security.PasswordManager;
+import di.uoa.gr.dira.services.customerService.CustomerService;
+import di.uoa.gr.dira.services.customerService.ICustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private CustomerService customerService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(customerService).passwordEncoder(PasswordManager.encoder());
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -23,5 +41,13 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                     .antMatchers("/").permitAll(); // Currently, all requests are authorized
 //                .anyRequest().authenticated(); // Every request except the authorized must be authenticated
+
+        http.addFilterBefore(new JwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+
+    }
+    @Bean(BeanIds.AUTHENTICATION_MANAGER)
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
