4a6fcbac4e325561256fd44ad66f8b3b5852022f
aspil <sdi1700147@di.uoa.gr>
2021-05-11 23:06:38 +0300

    Logic controller throws NullPointerException at Authentication instantiation

--- back-end/dira/src/main/java/di/uoa/gr/dira/controllers/LoginController.java
+++ back-end/dira/src/main/java/di/uoa/gr/dira/controllers/LoginController.java
@@ -1,25 +1,41 @@
 package di.uoa.gr.dira.controllers;
 
 import di.uoa.gr.dira.models.customer.CustomerLoginModel;
+import di.uoa.gr.dira.security.JwtProvider;
 import di.uoa.gr.dira.services.customerService.ICustomerService;
-import org.springframework.validation.annotation.Validated;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
-@Validated
 @RestController
+@RequestMapping("login")
 public class LoginController {
-    private final ICustomerService service;
 
-    public LoginController(ICustomerService service) {
-        this.service = service;
-    }
+//    @Autowired
+    AuthenticationManager authenticationManager;
 
-    @PostMapping("login")
-    public boolean login(@Valid @RequestBody CustomerLoginModel customerLoginModel) {
-        return service.authenticateUser(customerLoginModel.getUsername(), customerLoginModel.getPassword());
+//    @Autowired
+    JwtProvider jwtProvider;
+
+    @PostMapping()
+    public String login(@Valid @RequestBody CustomerLoginModel customerLoginModel) {
+//        return service.authenticateUser(customerLoginModel.getUsername(), customerLoginModel.getPassword());
+        Authentication auth = authenticationManager.authenticate( // TODO: fix NullPointerException
+                new UsernamePasswordAuthenticationToken(
+                        customerLoginModel.getUsername(),
+                        customerLoginModel.getPassword()
+                )
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(auth);
+        return jwtProvider.generateToken(auth);
     }
 }
