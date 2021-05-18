0b296fd6d6cdae53659a0ceb73efa01f158af17a
Nikitas <nikitasakkas@gmail.com>
2021-05-18 21:50:32 +0300

    project screens navbar with account settings button(form not ready)

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,9 +2,7 @@
 // import Home from './components/Home';
 // import Register from './components/Register';
 // import Login from './components/Login';
-import Password_Recovery_1 from './components/PasswordRecovery';
-// import Password_Recovery_2 from './components/Password_Recovery_2';
-// import Password_Recovery_3 from './components/Password_Recovery_3';
+// import Password_Recovery_1 from './components/PasswordRecovery';
 // import Plan from './components/Plans'
 
 import ProjectMain from './components/ProjectMain'
@@ -12,7 +10,7 @@ import ProjectMain from './components/ProjectMain'
 function App() {
   return (
     <div className="App">
-        <Password_Recovery_1/>
+        <ProjectMain/>
       {/* <Home/> */}
     </div>
   );
--- front-end/src/components/ProjectNav.js
+++ front-end/src/components/ProjectNav.js
@@ -1,40 +1,44 @@
+import accountIcon from "../Images/profile_icon.png"
+
+
 const ProjectNav = () => {
 
-  const open_settings = () => {
-    // document.getElementById("myForm").style.display = "block";
-    console.log("eeeeeo");
+  const openSettings = () => {
+    if(document.getElementById("myForm").style.display == "none"){
+      document.getElementById("myForm").style.display = "block";
+    }
+    else{
+      document.getElementById("myForm").style.display = "none";
+    }
   }
   
   function closeForm() {
     document.getElementById("myForm").style.display = "none";
-    console.log("eeeeeo");
 
   }
-  open_settings();
+  // openSettings();
   return (
     
     <div className="projectnav">
-      {/* <h1>Dira</h1>
+      <h1>Dira</h1>
       <div className="links">
         <a href="/pricing" className="boxxed_button">Go Premium</a>
         <a href="/contact">Contact</a>
-      </div> */}
+        <img src={accountIcon} alt="accountIcon" className="settingsPopupReference" onClick={openSettings}/>
 
-      <button className="open-button" onClick={open_settings}>Open Form</button>
-      {/* <div class="form-popup" id="myForm">
+        {/* <a  onClick={openSettings}>Open Form</a> */}
+      </div>
+      <div class="form-popup" id="myForm">
         <form action="/action_page.php" class="form-container">
           <h1>Login</h1>
-
           <label for="email"><b>Email</b></label>
           <input type="text" placeholder="Enter Email" name="email" required></input>
-
           <label for="psw"><b>Password</b></label>
           <input type="password" placeholder="Enter Password" name="psw" required></input>
-
           <button type="submit" class="btn">Login</button>
-          <button type="button" class="btn cancel" onclick={closeForm}>Close</button>
+          <button type="button" class="btn cancel" onClick={closeForm}>Close</button>
         </form>
-      </div> */}
+      </div>
 
       
     </div>
