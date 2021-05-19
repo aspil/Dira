559884075f705e30e28eaeb4b52be46495b25f62
FTheodore <theofil97@gmail.com>
2021-05-19 09:13:40 +0300

    side navigation animation

--- front-end/src/components/ProjectMain.js
+++ front-end/src/components/ProjectMain.js
@@ -1,10 +1,19 @@
 import ProjectNav from './ProjectNav';
+import SideNav from './SideNav';
 // import logo from "../Images/dira_icon.png"
 
 function Home() {
   return (
     <div className="projectmain">
       <ProjectNav/>
+      <div className="center">
+        <SideNav/>
+        <div className="content">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic incidunt blanditiis, esse sequi nulla dicta et veniam tenetur corrupti earum ducimus vel, similique expedita sit voluptatum accusantium minus ex aliquid!
+          </p>
+        </div>
+      </div>
 
     </div>
   );
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css'
 import './project_screens.css'
+import './side_navigation.css'
 
 
 ReactDOM.render(
