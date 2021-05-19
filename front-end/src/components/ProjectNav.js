cbd8def9b54dac5a871f3415fb75063a66c95933
Nikitas <nikitasakkas@gmail.com>
2021-05-19 04:05:51 +0300

    project screens navbar with account settings button(form ready)

--- front-end/src/components/ProjectNav.js
+++ front-end/src/components/ProjectNav.js
@@ -11,11 +11,7 @@ const ProjectNav = () => {
       document.getElementById("myForm").style.display = "none";
     }
   }
-  
-  function closeForm() {
-    document.getElementById("myForm").style.display = "none";
 
-  }
   // openSettings();
   return (
     
@@ -29,14 +25,11 @@ const ProjectNav = () => {
         {/* <a  onClick={openSettings}>Open Form</a> */}
       </div>
       <div class="form-popup" id="myForm">
-        <form action="/action_page.php" class="form-container">
-          <h1>Login</h1>
-          <label for="email"><b>Email</b></label>
-          <input type="text" placeholder="Enter Email" name="email" required></input>
-          <label for="psw"><b>Password</b></label>
-          <input type="password" placeholder="Enter Password" name="psw" required></input>
-          <button type="submit" class="btn">Login</button>
-          <button type="button" class="btn cancel" onClick={closeForm}>Close</button>
+        <form class="form-container">
+          <p>"Username"</p>
+
+          <button type="submit" class="btn">Upgrade</button>
+          <button type="button" class="btn">Logout</button>
         </form>
       </div>
 
