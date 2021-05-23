83b2f6f476983b057c546675a6765a40e263a89e
FTheodore <theofil97@gmail.com>
2021-05-23 20:27:15 +0300

    login state

--- front-end/src/components/Login.js
+++ front-end/src/components/Login.js
@@ -1,28 +1,49 @@
 import { Link } from "react-router-dom";
 import logo from "../Images/dira_icon.png"
+// import dira_client from "dira-clients";
+import { useState } from "react";
+
+// const client = 
 
 const Login = () => {
-    return (
-      <div className="login" >
-        <div style={{textAlign:"center"}}>
-            <img src={logo} alt="dira logo" id="dira logo"/>
-            <div className="login_grad" style={{textAlign:"center"}}>
-                <h1 style={{fontWeight:"normal", margin:"15px"}}>Login</h1>
-                
-                <input type="text" placeholder="Username or email address"></input>
-                <input type="text" placeholder="Password"></input>
-                <div style={{textAlign:"right", marginRight:"1.8vw"}}>
-                <Link to="/recover" >Forgot Password?</Link>
-                </div>
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-                <p><button>Login</button></p>
-                <p style={{margin: "15px"}}>Don't have an account? <Link to="/register">Register</Link></p>
-                <Link to="/contact">Contact Us</Link>
-            </div>
-        </div>
+  const onSubmit = (e) => {
+    e.preventDefault();
 
+    console.log(email, password);
+  }
+  return (
+    <div className="login" >
+      <div style={{textAlign:"center"}}>
+          <img src={logo} alt="dira logo" id="dira logo"/>
+          <div className="login_grad" style={{textAlign:"center"}}>
+              <h1 style={{fontWeight:"normal", margin:"15px"}}>Login</h1>
+              <form onSubmit={onSubmit}>
+                <input 
+                  type="email" 
+                  placeholder="Email address" 
+                  onChange = {(e) => {setEmail(e.target.value)}}
+                  value={email}/>
+                <input 
+                  type="password" 
+                  placeholder="Password" 
+                  onChange = {(e) => {setPassword(e.target.value)}}
+                  value={password}/>
+                <button type="submit">Login</button>
+              </form>
+              <div style={{textAlign:"right", marginRight:"1.8vw"}}>
+              <Link to="/recover" >Forgot Password?</Link>
+              </div>
+
+              <p style={{margin: "15px"}}>Don't have an account? <Link to="/register">Register</Link></p>
+              <Link to="/contact">Contact Us</Link>
+          </div>
       </div>
-    );
+
+    </div>
+  );
   }
    
   export default Login;
