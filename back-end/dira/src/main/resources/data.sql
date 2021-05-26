9ad4ae28d3cc70a09e5f45619bd212bf0ba52d80
George Liontos <sdi1600094@di.uoa.gr>
2021-05-26 21:50:16 +0300

    Added default user in backend during db initialization

--- back-end/dira/src/main/resources/data.sql
+++ back-end/dira/src/main/resources/data.sql
@@ -6,3 +6,20 @@ insert into subscription_plan(subscription_plan_id, plan) values (2, 'PREMIUM');
 insert into issue_type(issue_type_id, type) values(1, 'Epic');
 insert into issue_type(issue_type_id, type) values(2, 'Story');
 insert into issue_type(issue_type_id, type) values(3, 'Defect');
+
+-- Insert customer static data
+insert into customer(
+    username,
+    name,
+    surname,
+    email,
+    password,
+    subscription_plan_id
+) values (
+    'tester',
+    'Tester',
+    'Mc Tester',
+    'test@otenet.gr',
+    '$2a$10$bb/RXy.KPolaCeW14ADg0eC9.kchxyHCu5T5m.C1GBHkA.vdK5ST.', -- 12345678
+    1
+);
