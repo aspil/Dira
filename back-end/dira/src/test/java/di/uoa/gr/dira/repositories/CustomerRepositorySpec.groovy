6b7f3c14ba674af062b63895702605c9a6d7b6b2
George Liontos <sdi1600094@di.uoa.gr>
2021-06-09 00:27:47 +0300

    [backend]: Added customer repository test

--- back-end/dira/src/test/java/di/uoa/gr/dira/repositories/CustomerRepositorySpec.groovy
+++ back-end/dira/src/test/java/di/uoa/gr/dira/repositories/CustomerRepositorySpec.groovy
@@ -7,10 +7,12 @@ import org.jeasy.random.EasyRandomParameters
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.annotation.DirtiesContext
 import spock.lang.Specification
 
 @SpringBootTest
 @AutoConfigureTestDatabase
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class CustomerRepositorySpec extends Specification {
     @Autowired
     private CustomerRepository repository
@@ -29,5 +31,24 @@ class CustomerRepositorySpec extends Specification {
         then: "The customer is saved successfully"
         saved
         saved.getId() == 2L
+    }
+
+    void "insert two customers works"() {
+        given: "I have two customers"
+        EasyRandomParameters parameters = new EasyRandomParameters()
+        parameters.setCollectionSizeRange(new EasyRandomParameters.Range<Integer>(0, 0))
+        List<Customer> customers = ObjectGenerator.generateObjectList(Customer.class, parameters, 2)
+        customers[0].setId(null)
+        customers[0].setSubscriptionPlanFromEnum(SubscriptionPlanEnum.STANDARD)
+        customers[1].setId(null)
+        customers[1].setSubscriptionPlanFromEnum(SubscriptionPlanEnum.PREMIUM)
+
+        when: "I insert the customers to the database"
+        List<Customer> saved = repository.saveAll(customers)
+
+        then: "The customers are saved successfully"
+        saved.size() == 2
+        saved[0].getId() == 2L
+        saved[1].getId() == 3L
     }
 }
--- back-end/dira/src/test/java/di/uoa/gr/dira/utils/ObjectGenerator.java
+++ back-end/dira/src/test/java/di/uoa/gr/dira/utils/ObjectGenerator.java
@@ -3,6 +3,7 @@ package di.uoa.gr.dira.utils;
 import org.jeasy.random.EasyRandom;
 import org.jeasy.random.EasyRandomParameters;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -17,5 +18,15 @@ public class ObjectGenerator {
 
     public static <T> List<T> generateObjectList(Class<T> clazz, int numberOfObjects) {
         return new EasyRandom().objects(clazz, numberOfObjects).collect(Collectors.toList());
+    }
+
+    public static <T> List<T> generateObjectList(Class<T> clazz, EasyRandomParameters params, int numberOfObjects) {
+        List<T> res = new ArrayList<>(numberOfObjects);
+
+        for (int i = 0; i != numberOfObjects; ++i) {
+            res.add(generateObject(clazz, params));
+        }
+
+        return res;
     }
 }
