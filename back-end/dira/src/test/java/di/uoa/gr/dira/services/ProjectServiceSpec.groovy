cd9a21e3a40de6960f3e75a36fcd9997fb7487c7
themisvr <varveristhemis@gmail.com>
2021-06-09 13:17:25 +0300

    [backend]: plan related tests on project service

--- back-end/dira/src/test/java/di/uoa/gr/dira/services/ProjectServiceSpec.groovy
+++ back-end/dira/src/test/java/di/uoa/gr/dira/services/ProjectServiceSpec.groovy
@@ -1,13 +1,18 @@
 package di.uoa.gr.dira.services
 
 import di.uoa.gr.dira.configuration.ModelMapperConfiguration
+import di.uoa.gr.dira.entities.customer.Customer
+import di.uoa.gr.dira.entities.customer.SubscriptionPlan
 import di.uoa.gr.dira.entities.project.Project
+import di.uoa.gr.dira.exceptions.commonExceptions.ActionNotPermittedException
+import di.uoa.gr.dira.exceptions.customer.CustomerNotFoundException
 import di.uoa.gr.dira.models.project.ProjectModel
 import di.uoa.gr.dira.repositories.CustomerRepository
 import di.uoa.gr.dira.repositories.PermissionRepository
 import di.uoa.gr.dira.repositories.ProjectRepository
 import di.uoa.gr.dira.services.projectService.ProjectService
 import di.uoa.gr.dira.shared.ProjectVisibility
+import di.uoa.gr.dira.shared.SubscriptionPlanEnum
 import di.uoa.gr.dira.utils.ObjectGenerator
 import org.modelmapper.ModelMapper
 import org.springframework.beans.factory.annotation.Autowired
@@ -43,5 +48,50 @@ class ProjectServiceSpec extends Specification {
         publicProjects.size() == 2
         publicProjects[0] == mapper.map(projects[0], ProjectModel.class)
         publicProjects[1] == mapper.map(projects[2], ProjectModel.class)
+    }
+
+    void "get public project when customer has STANDARD plan"() {
+        given: "We have a public project"
+        Project project = ObjectGenerator.generateObject(Project.class)
+        project.setVisibility(ProjectVisibility.PUBLIC)
+        projectRepository.find() >> project
+
+        and: "We have a customer with STANDARD plan"
+        Customer customer = ObjectGenerator.generateObject(Customer.class)
+        customer.setSubscriptionPlanFromEnum(SubscriptionPlanEnum.STANDARD)
+        customerRepository.findByUsername("test") >> Optional.of(customer)
+
+        when: "The customer requests to retrieve the public project"
+        SubscriptionPlanEnum subscriptionPlanEnum = customerRepository.findByUsername("test").get().getSubscriptionPlan().getPlan()
+        Optional<ProjectModel> optionalProjectModel = service.getProject(project.getId(), subscriptionPlanEnum) as Optional<ProjectModel>
+
+        then: "We get the project"
+        optionalProjectModel.isPresent()
+        ProjectModel projectModel = optionalProjectModel.get()
+        projectModel.description == project.description
+        projectModel.id == project.id
+        projectModel.key == project.key
+        projectModel.name == project.name
+        projectModel.visibility == project.visibility
+    }
+
+    void "return exception when customer with STANDARD plan requests for private project"() {
+        given: "We have a private project"
+        Project project = ObjectGenerator.generateObject(Project.class)
+        project.setVisibility(ProjectVisibility.PRIVATE)
+        projectRepository.find() >> project
+
+        and: "We have a customer with STANDARD plan"
+        Customer customer = ObjectGenerator.generateObject(Customer.class)
+        customer.setSubscriptionPlanFromEnum(SubscriptionPlanEnum.STANDARD)
+        customerRepository.findByUsername("test") >> Optional.of(customer)
+
+        when: "The customer requests to retrieve the private project"
+        SubscriptionPlanEnum subscriptionPlanEnum = customerRepository.findByUsername("test").get().getSubscriptionPlan().getPlan()
+        Optional<ProjectModel> optionalProjectModel = service.getProject(project.getId(), subscriptionPlanEnum) as Optional<ProjectModel>
+
+        then: "I get an error that action not permitted"
+        ActionNotPermittedException ex = thrown(ActionNotPermittedException)
+        ex.message == "Action Not Permitted!"
     }
 }
