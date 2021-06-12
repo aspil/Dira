63c826c712993aa89032b94ecfc332294946e196
themisvr <varveristhemis@gmail.com>
2021-06-12 17:49:21 +0300

    [backend]: dont ever change this again

--- back-end/dira/src/main/java/di/uoa/gr/dira/DiraApplication.java
+++ back-end/dira/src/main/java/di/uoa/gr/dira/DiraApplication.java
@@ -28,7 +28,7 @@ public class DiraApplication {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+                registry.addMapping("/**").allowedOrigins("https://localhost:3000");
             }
         };
     }
