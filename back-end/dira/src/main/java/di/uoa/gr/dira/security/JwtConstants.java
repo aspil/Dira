730dadfcb561af73a927db1142755f72c1d15082
George Liontos <sdi1600094@di.uoa.gr>
2021-06-24 17:38:52 +0300

    [backend]: Set the JWT expiration time to 2 hours

--- back-end/dira/src/main/java/di/uoa/gr/dira/security/JwtConstants.java
+++ back-end/dira/src/main/java/di/uoa/gr/dira/security/JwtConstants.java
@@ -1,7 +1,7 @@
 package di.uoa.gr.dira.security;
 
 public class JwtConstants {
-    public static final long EXPIRATION_TIME_MS = Long.MAX_VALUE; // 15 minutes
+    public static final long EXPIRATION_TIME_MS = 7_200_000; // 2 hours
     public static final String SECRET = "DIRA_SECRET_JWT_KEY";
     public static final String TOKEN_PREFIX = "Bearer ";
 }
