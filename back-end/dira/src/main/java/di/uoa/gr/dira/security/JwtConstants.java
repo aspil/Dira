811951f12be7127ec1f990625b2083059e6651dd
themisvr <varveristhemis@gmail.com>
2021-06-24 15:18:26 +0300

    [backend]: extended expiration time of JWT token

--- back-end/dira/src/main/java/di/uoa/gr/dira/security/JwtConstants.java
+++ back-end/dira/src/main/java/di/uoa/gr/dira/security/JwtConstants.java
@@ -1,7 +1,7 @@
 package di.uoa.gr.dira.security;
 
 public class JwtConstants {
-    public static final long EXPIRATION_TIME_MS = 900_000; // 15 minutes
+    public static final long EXPIRATION_TIME_MS = Long.MAX_VALUE; // 15 minutes
     public static final String SECRET = "DIRA_SECRET_JWT_KEY";
     public static final String TOKEN_PREFIX = "Bearer ";
 }
