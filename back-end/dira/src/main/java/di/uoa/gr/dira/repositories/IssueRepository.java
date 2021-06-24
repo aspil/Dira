8c29fdeda7b3d4943e9cdbc9a84e83705865ae23
George Liontos <sdi1600094@di.uoa.gr>
2021-06-24 21:00:32 +0300

    [backend]: Third take on UpdateIssue method

--- back-end/dira/src/main/java/di/uoa/gr/dira/repositories/IssueRepository.java
+++ back-end/dira/src/main/java/di/uoa/gr/dira/repositories/IssueRepository.java
@@ -4,6 +4,9 @@ import di.uoa.gr.dira.entities.issue.Issue;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface IssueRepository extends JpaRepository<Issue, Long> {
+    Optional<Issue> findByKey(String key);
 }
