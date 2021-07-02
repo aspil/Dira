ded1788abb89ab3c0f8bf988549ba366b9cd923a
FTheodore <theofil97@gmail.com>
2021-07-02 08:53:09 +0300

    [frontend]: password recovery validation and refactoring

--- front-end/src/components/PasswordRecovery.js
+++ front-end/src/components/PasswordRecovery.js
@@ -11,6 +11,10 @@ const PasswordRecovery = ({ navHandle, userClientRef }) => {
   const history = useHistory();
   const [email, setEmail] = useState('');
   const [emailError, setEmailError] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [pin, setPin] = useState('');
+  const [recoveryError, setRecoveryError] = useState('');
 
   const sendEmail = () => {
     if (!email) {
@@ -23,25 +27,37 @@ const PasswordRecovery = ({ navHandle, userClientRef }) => {
     }
     setEmailError('');
 
-    userClientRef.current.reset_password(email).catch(console.log);
-    setStep(2);
-  }
-  // const checkCode = () => {
-  //   setStep(2);
-  // }
+    userClientRef.current.reset_password(email)
+      .then(() => {
+        setStep(2);
+      })
+      .catch(err => {
+        console.log(err);
+        setEmailError('Couldn\'t send recovery email');
+      });
+  };
+
   const resetPassword = () => {
-    setStep(3);
+    if (!(pin && password && confirmPassword)) {
+      setRecoveryError('Please fill in all fields');
+      return;
+    }
+    else if (password !== confirmPassword) {
+      setRecoveryError('Passwords don\'t match');
+      return;
+    }
+    setRecoveryError('');
+
+    // TO-DO
+    setRecoveryError('Not Available');
   }
 
-  const redirectToMain = () => {
-    history.push('/')
-  }
   useEffect(navHandle, [navHandle]);
 
   return (
     <div className="login">
       <div style={{ textAlign: "center" }}>
-        <img src={logo} alt="dira logo" id="dira logo" onClick={redirectToMain} />
+        <img src={logo} alt="dira logo" id="dira logo" onClick={() => history.push('/')} />
         <div className="login_grad" style={{ textAlign: "center" }}>
           <img src={arrows} alt="arrows" style={{ width: "90px" }} />
           <h1 style={{ fontWeight: "normal", marginBottom: "40px" }}>Password Recovery</h1>
@@ -58,19 +74,30 @@ const PasswordRecovery = ({ navHandle, userClientRef }) => {
               <p><button onClick={sendEmail}>Continue</button></p>
             </div>
           }
-          {/* {step === 2 &&
-            <div>
-              <p className="inputHead" style={{ textAlign: 'left'}}>Enter e-mail address:</p>
-              <input type="text" placeholder="Code"></input>
-              <p><button onClick={checkCode}>Continue</button></p>
-            </div>
-          } */}
           {step === 2 &&
             <div>
+              <p className="inputHead" style={{ textAlign: 'left' }}>Pin inside email:</p>
+              <input
+                type="text"
+                placeholder="Pin"
+                value={pin}
+                onChange={e => setPin(e.target.value)}
+              />
               <p className="inputHead" style={{ textAlign: 'left' }}>New password:</p>
-              <input type="text" placeholder="Password"></input>
+              <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+              />
               <p className="inputHead" style={{ textAlign: 'left' }}>Confirm password:</p>
-              <input type="text" placeholder="Confirm Password"></input>
+              <input
+                type="password"
+                placeholder="Password"
+                value={confirmPassword}
+                onChange={e => setConfirmPassword(e.target.value)}
+              />
+              {Boolean(recoveryError) && <p style={{ color: 'crimson' }}>{recoveryError}</p>}
               <p><button onClick={resetPassword} >Update Password</button></p>
             </div>
           }
